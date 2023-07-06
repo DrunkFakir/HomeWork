dbcb72404f295ddb85b0d6717657f89f9020db86
JoJo <jovannyclon@gmail.com>
2023-07-06 14:33:23 +0500

    Исправил орфографическую ошибку

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -4,9 +4,9 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
-Console.WriteLine("ВВедите первое число");
+Console.WriteLine("Введите первое число");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("ВВедите второе число");
+Console.WriteLine("Введите второе число");
 int b = Convert.ToInt32(Console.ReadLine());
 
 if (a > b)
