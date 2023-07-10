41e46585338674fcb3555474fd111ba2b8f8f1ab
JoJo <jovannyclon@gmail.com>
2023-07-10 18:20:21 +0500

    Добавил задание 9

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -4,9 +4,21 @@
 // 78 -> 8
 // 12-> 2
 // 85 -> 8
-int number = new Random().Next(10, 100); // 99 + 1 = 100
+int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из диапазона 10 - 99 -> {number}");
-int firstDigit = number / 10 ; //  78 / 10 = 7
-int secondDigit = number % 10; // 78 == 70 + 8
-if (firstDigit > secondDigit) Console.WriteLine($"Максимальная цифра числа -> {firstDigit}");
-else Console.WriteLine($"Иаксимальная цифра числа -> {secondDigit}");
+// int firstDigit = number / 10 ; //  78 / 10 = 7
+// int secondDigit = number % 10; // 78 == 70 + 8
+// if (firstDigit > secondDigit) Console.WriteLine($"Максимальная цифра числа -> {firstDigit}");
+// else Console.WriteLine($"Максимальная цифра числа -> {secondDigit}");
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Максимальная цифра числа -> {maxDigit}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    // if (firstDigit > secondDigit) return firstDigit;
+    // return secondDigit;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
