9a9861494081608a21f899610da250f1ca62a824
JoJo <jovannyclon@gmail.com>
2023-08-02 17:49:42 +0500

    Сделал задание 58

---
+++ Task58/Program.cs
@@ -0,0 +1,78 @@
+﻿// =Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] array2d = CreateMatrixRndInt(3, 3, 1, 9);
+PrintMatrix(array2d);
+Console.WriteLine(" ");
+int[,] arr2d = CreateMatrixRndInt(3, 3, 1, 9);
+PrintMatrix(arr2d);
+
+Console.WriteLine(" ");
+int[,] resultMatrix = new int[3, 3];
+MultiplyMatrix(array2d, arr2d, resultMatrix);
+PrintMatrixResult(resultMatrix);
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+void PrintMatrixResult(int[,] matrix)
+{
+    Console.WriteLine($"Произведение двух матриц: ");
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
