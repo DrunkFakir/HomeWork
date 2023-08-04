598a762fbc2af45705ca1562f1d6300e642f31ee
JoJo <jovannyclon@gmail.com>
2023-08-04 16:30:58 +0500

    Задача 67

---
+++ Task67/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр. 
+// 453 -> 12 
+// 45 -> 9 
+
+// int FactorialRec(int n)
+// {
+//     if (n == 1) return 1;
+//     return n * FactorialRec(n - 1);   Нахождение факториала
+// }
+// Console.WriteLine(FactorialRec(10));
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = SumDigit(number);
+Console.WriteLine($"{sum}");
+
+int SumDigit(int n)
+{
+    if (n == 0) return 0;
+    return n % 10 + SumDigit(n / 10);  //  (К примеру число) 453 -> 45 -> 4 | 0 + 4 % 10 + 45 % 10 + 453 % 10 = 12
+}
